ff52b575e6870b2e37222df5297abf9d1da38c6f
danyos <daniel.hambardzumyan@mail.ru>
2023-07-04 22:39:29 +0400

    Javascript first lesson

---
+++ js-1/script.js
@@ -0,0 +1,73 @@
+// variable
+// let x=111
+// const green="#00ff77"
+// green="#10ff77"
+// green=114
+// console.log(green)
+
+// console.log(x)
+//
+//
+//  x=x+1
+//
+// console.log(x)
+
+// let x=70
+// let y=100
+// let z=0
+// z=y//10
+// y=x
+// x=z
+
+// console.log(x)//10
+// console.log(y)//7
+
+
+// unenq 2 popoxakan
+// x ev y
+// vortex
+
+// let x=8
+// let y=20
+// // popoxel arjeqneri textery
+//
+// // ....
+// console.log(x)
+// console.log(y)
+
+// let a=10
+// a=a-1//9
+// a-=1 // 8
+// a/=2 // 4
+// console.log(a)
+
+// let b=30
+// // a-=b//-20
+// a=b-a //20
+
+a+=1
+console.log(a)
+a=a+1
+console.log(a)
+a++
+console.log(a)
+++a
+console.log(a)
+
+// Number // 1+1 * - / ** ++ -- %
+
+// console.log(1+1) // gumarum e irar result 2
+// console.log(2*3) // 6
+// console.log(8/2) // 4
+// console.log(18-5) // 13
+// console.log(198%5) //3  || 227%10->220->7 / 1,2,3,4,5,6??
+// console.log(2**5)// 2*2*2*2*2
+
+// String "texteri"
+// Boolean true / false
+// undefined
+// null
+// object {}
+
+// ereqshabi hingshabi shabat 21.00
+
