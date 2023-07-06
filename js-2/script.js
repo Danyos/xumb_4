03131751df0bdc640e41124f20b8d9ac5eaa802e
danyos <daniel.hambardzumyan@mail.ru>
2023-07-06 23:06:36 +0400

    javascript number and string

---
+++ js-2/script.js
@@ -0,0 +1,72 @@
+// // "use strict";
+// let UserInterfaces=1
+// let name="valod"
+// let Surname=`barev ${name} jan vonces`
+//  user_email=5
+// let __arjeq__=7
+// __arjeq__+=2
+
+
+// console.log(window)
+// alert(window)
+// alert("barev2")
+// alert("barev3")
+// alert("barev4")
+// alert("barev5")
+// alert("barev6")
+// console.log("hajox")
+// let name=prompt("greq anun")//"Mxitar"
+//
+//
+// let x="UNI MI METS  SHUN VORY PARBERABAR"
+// let y="Andatar lacum e u lacum"
+// x=name+" "+ x + " HACHUM E." + " " + y
+// // number+string=string(concat)
+//
+// console.log(x)
+
+
+
+
+//
+// let fast=prompt("aragutyun")
+// console.log(fast)
+// // let km=prompt("km")
+// // let x=parseInt(km/fast)
+// // let y=km%fast
+// // console.log(x,y)
+//
+// let a= parseInt(prompt("grir 1 in tiv@")) //7
+// let b= parseInt(prompt("grir 2 in tiv@"))//5
+// a=a+b //12
+// b= a-b// 7
+// a=a-b // 5
+// console.log("a =",a)
+// console.log("b =",b)
+
+
+// ____________Number_______________
+//     parseInt()
+// let num='98657545'
+//
+// console.log(num.length>=9)
+
+
+
+
+
+// ____________String_______________
+//     "scad".length // hashvum er qanaky
+
+
+
+
+
+
+
+
+// tnayin nayel u angir
+// https://javascript.info/type-conversions
+
+
+
