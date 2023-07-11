887b10f46a8b26d02eb08d8f78837ffb6938b6f8
danyos <daniel.hambardzumyan@mail.ru>
2023-07-11 22:59:12 +0400

    string methods

---
+++ js-3/script.js
@@ -0,0 +1,19 @@
+// let x="700$";
+// let y="390d";
+// console.log(parseFloat(x)*parseFloat(y))
+// number + string// concat
+// x * y
+// let z=x+(y*1)
+// let z=+x + +y
+// 'b'*"cxd"
+// parseInt("232$")
+// console.log(z)
+// _________
+// let x=8
+// let y="8"
+// console.log(x!==y)
+
+
+let x=+"-1000-"
+console.log(x>0 || x<0)
+
---
+++ js-4/script.js
@@ -0,0 +1,74 @@
+// 0;
+// null
+// ""
+//  undefined,
+//NaN,
+//false
+
+
+
+// alert(confirm('Haskacar')&&confirm('Mi ban jogir'))
+
+
+// "778"<"12356" // true || false
+
+
+// (false || 18 && 0) || (10 && true) && (10 || false)
+
+// ____________Trim______________
+// let str=prompt("grir msg").trim()
+// console.log(str)
+// __________slice_________________
+// let str="10+10-20*9"
+
+// console.log(str.slice(-1) === "/" ||
+//     str.slice(-1) === "*" ||
+//     str.slice(-1) === "-"||
+//     str.slice(-1) === "+" )
+
+// ____________toUpperCase_____________
+
+// let str="Poxos"
+//
+//
+// let str1="PoXoS"
+//
+// let str2="poxos"
+// let str3="poxos"
+//
+//
+//
+//
+// let search="POXOS"
+// console.log(search.toLowerCase().trim() === str2.toLowerCase())
+
+let str="barev axper jan yes unem Spitak 071g9"
+console.log(str.charCodeAt(str.length-1)>=48 && str.charCodeAt(str.length-1)<=57)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
